c6004c4b95514ba3cf93d1b997ce45e2924a5d78
Shakilofficial <mrshakilhossainofficial@gmail.com>
2025-03-01 23:45:10 +0600

    send token in cookies after register student and tutor

--- backend/src/app/modules/user/user.controller.ts
+++ backend/src/app/modules/user/user.controller.ts
@@ -1,8 +1,9 @@
 import { StatusCodes } from 'http-status-codes';
+import config from '../../config';
 import { IImageFile } from '../../interface/IImageFile';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
-
+import { authServices } from '../auth/auth.service';
 import { userServices } from './user.service';
 
 const createStudent = catchAsync(async (req, res) => {
@@ -15,14 +16,33 @@ const createStudent = catchAsync(async (req, res) => {
     profileImage,
   );
 
+  // Generate tokens after successful creation
+  const { accessToken, refreshToken } = await authServices.loginUser({
+    email: userData.email,
+    password: userData.password,
+  });
+
+  // Set refreshToken in cookie
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+    sameSite: 'none',
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+  });
+
   sendResponse(res, {
     statusCode: StatusCodes.CREATED,
     success: true,
     message: 'Student created successfully',
-    data: result,
+    data: {
+      accessToken,
+      user: result.user,
+      student: result.student,
+    },
   });
 });
 
+// Tutor controller
 const createTutor = catchAsync(async (req, res) => {
   const { name, email, password, ...tutorData } = req.body;
   const profileImage = req.file as IImageFile;
@@ -33,11 +53,29 @@ const createTutor = catchAsync(async (req, res) => {
     profileImage,
   );
 
+  // Generate tokens after successful creation
+  const { accessToken, refreshToken } = await authServices.loginUser({
+    email,
+    password,
+  });
+
+  // Set refreshToken in cookie
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+    sameSite: 'none',
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+  });
+
   sendResponse(res, {
     statusCode: StatusCodes.CREATED,
     success: true,
     message: 'Tutor created successfully',
-    data: result,
+    data: {
+      accessToken,
+      user: result.user,
+      tutor: result.tutor,
+    },
   });
 });
 
